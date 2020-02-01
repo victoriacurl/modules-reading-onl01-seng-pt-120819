6b938a36fbbfd4b810899301a875c4d01a0535ba
Victoria Curl <victoriacurl9@gmail.com>
2020-02-01 01:30:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,11 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
 end
